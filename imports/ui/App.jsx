740fd3bbdbc2a5b6a8c8b198027ef2eb719dc076
Varun Babu Pozhath <varunbabu008@gmail.com>
2017-01-12 01:27:53 +1100

    started player component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,13 +2,25 @@ import React, {Component} from 'react';
 //import {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import TeamList from './Team-list'; 
+import Teamstats from './Team-stats';
+import Player from './Player'
 
 export default class App extends Component{
   render(){
     return (
       <MuiThemeProvider>
-        <RaisedButton label="Oh yeah we did it" />
-          
+        <div className="container">
+          <AppBar title="Soccer Application"
+          iconClassNameRight = "muidocs-icon-navigation-expand-more"
+          showMenuIconButton={false}/>
+          <div className="row">
+            <div className = "col s12 m7"><Player/></div>
+            <div className = "col s12 m5"><Teamstats/></div>
+            <div className = "col s12 m5"><TeamList/></div>
+          </div>
+        </div>
       </MuiThemeProvider>
     )
   }
--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
+import {Card,CardMedia,CardTitle,CardText,CardActions} from 'material-ui/Card';
 
 export default class Player extends Component{
   render(){
     return (
-     <h1>Player</h1>
+     <Card>
+    
+    <CardMedia
+      overlay={<CardTitle title="Varun" subtitle="Offense: 12 - Defense: 8" />}
+    >
+      <img src="player.jpg" />
+    </CardMedia>
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      
+    </CardActions>
+  </Card>
     )
   }
 }
